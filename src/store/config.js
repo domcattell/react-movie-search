99d9813c9342b7736c5824770fc1fc02356c86be
domcattell <55451364+domcattell@users.noreply.github.com>
2020-07-06 16:19:03 +0100

    minor changes to config

---
+++ src/store/config.js
@@ -0,0 +1,5 @@
+//this file should really be in .gitignore but considering it's a free to use API
+//and very easy to get a key I have left it visible
+
+export const API_KEY = "&apikey=e0027f93";
+export const BASE_URI = "http://www.omdbapi.com/"
