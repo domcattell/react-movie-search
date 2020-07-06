4d9941c4f7e5d94c7c42ad9b985c66f57f36f8c4
domcattell <55451364+domcattell@users.noreply.github.com>
2020-07-06 16:14:40 +0100

    added page not found component

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import FilmPage from '../pages/FilmPage';
 import SearchPage from '../pages/SearchPage';
+import PageNotFound from '../pages/PageNotFound';
 
 const Routes = () => {
     return (
@@ -10,6 +11,7 @@ const Routes = () => {
             <Route exact path='/' component={Homepage}/>
             <Route exact path='/movies/:filmID' component={FilmPage}/>
             <Route exact path='/movies/search/:query' component={SearchPage}/>
+            <Route component={PageNotFound}/>
         </Switch>
     );
 }
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,16 @@
+ import React from 'react';
+ import {Container, Box, Typography} from '@material-ui/core';
+ 
+ const PageNotFound = () => {
+     return (
+         <Box display="flex" justifyContent="center" alignItems="center">
+             <Container>
+                <Typography variant="h3">404 - Page not found</Typography>
+            </Container>
+         </Box>
+    
+     );
+ }
+ 
+ export default PageNotFound;
+ 
